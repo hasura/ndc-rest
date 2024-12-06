793c5e571e211e96388f209d4b945bd1e8153cf8
Toan Nguyen <hgiasac@gmail.com>
2024-12-06 11:13:37 +0700

    wip: argument presets schema

--- ndc-http-schema/schema/setting.go
+++ ndc-http-schema/schema/setting.go
@@ -1,18 +1,24 @@
 package schema
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
+	"slices"
 	"strings"
 
 	"github.com/hasura/ndc-sdk-go/utils"
+	"github.com/invopop/jsonschema"
+	orderedmap "github.com/wk8/go-ordered-map/v2"
+	"gopkg.in/yaml.v3"
 )
 
 // NDCHttpSettings represent global settings of the HTTP API, including base URL, headers, etc...
 type NDCHttpSettings struct {
 	Servers         []ServerConfig             `json:"servers"                   mapstructure:"servers"         yaml:"servers"`
 	Headers         map[string]utils.EnvString `json:"headers,omitempty"         mapstructure:"headers"         yaml:"headers,omitempty"`
+	ArgumentPresets []ArgumentPreset           `json:"argumentPresets,omitempty" mapstructure:"argumentPresets" yaml:"argumentPresets,omitempty"`
 	SecuritySchemes map[string]SecurityScheme  `json:"securitySchemes,omitempty" mapstructure:"securitySchemes" yaml:"securitySchemes,omitempty"`
 	Security        AuthSecurities             `json:"security,omitempty"        mapstructure:"security"        yaml:"security,omitempty"`
 	Version         string                     `json:"version,omitempty"         mapstructure:"version"         yaml:"version,omitempty"`
@@ -46,6 +52,7 @@ func (rs *NDCHttpSettings) Validate() error {
 type ServerConfig struct {
 	URL             utils.EnvString            `json:"url"                       mapstructure:"url"             yaml:"url"`
 	ID              string                     `json:"id,omitempty"              mapstructure:"id"              yaml:"id,omitempty"`
+	ArgumentPresets []ArgumentPreset           `json:"argumentPresets,omitempty" mapstructure:"argumentPresets" yaml:"argumentPresets,omitempty"`
 	Headers         map[string]utils.EnvString `json:"headers,omitempty"         mapstructure:"headers"         yaml:"headers,omitempty"`
 	SecuritySchemes map[string]SecurityScheme  `json:"securitySchemes,omitempty" mapstructure:"securitySchemes" yaml:"securitySchemes,omitempty"`
 	Security        AuthSecurities             `json:"security,omitempty"        mapstructure:"security"        yaml:"security,omitempty"`
@@ -91,6 +98,277 @@ func (ss ServerConfig) GetURL() (*url.URL, error) {
 	return urlValue, nil
 }
 
+// ArgumentPresetValue represents an argument preset value type.
+type ArgumentPresetValueType string
+
+const (
+	ArgumentPresetValueTypeLiteral       ArgumentPresetValueType = "literal"
+	ArgumentPresetValueTypeEnv           ArgumentPresetValueType = "env"
+	ArgumentPresetValueTypeForwardHeader ArgumentPresetValueType = "forwardHeader"
+)
+
+var argumentPresetValueType_enums = []ArgumentPresetValueType{
+	ArgumentPresetValueTypeLiteral,
+	ArgumentPresetValueTypeEnv,
+	ArgumentPresetValueTypeForwardHeader,
+}
+
+// JSONSchema is used to generate a custom jsonschema
+func (j ArgumentPresetValueType) JSONSchema() *jsonschema.Schema {
+	return &jsonschema.Schema{
+		Type: "string",
+		Enum: toAnySlice(argumentPresetValueType_enums),
+	}
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (j *ArgumentPresetValueType) UnmarshalJSON(b []byte) error {
+	var rawResult string
+	if err := json.Unmarshal(b, &rawResult); err != nil {
+		return err
+	}
+
+	result, err := ParseArgumentPresetValueType(rawResult)
+	if err != nil {
+		return err
+	}
+
+	*j = result
+
+	return nil
+}
+
+// ParseArgumentPresetValueType parses ArgumentPresetValueType from string
+func ParseArgumentPresetValueType(value string) (ArgumentPresetValueType, error) {
+	result := ArgumentPresetValueType(value)
+	if !slices.Contains(argumentPresetValueType_enums, result) {
+		return result, fmt.Errorf("invalid ArgumentPresetValueType. Expected %+v, got <%s>", argumentPresetValueType_enums, value)
+	}
+
+	return result, nil
+}
+
+// ArgumentPreset represents an argument preset configuration,
+type ArgumentPreset struct {
+	Argument string              `json:"argument" mapstructure:"argument" yaml:"argument"`
+	Value    ArgumentPresetValue `json:"value"    mapstructure:"value"    yaml:"value"`
+}
+
+// ArgumentPresetValue represents an argument preset value information.
+type ArgumentPresetValue struct {
+	inner ArgumentPresetValueInterface
+}
+
+// Interface returns the inner interface.
+func (j ArgumentPresetValue) Interface() ArgumentPresetValueInterface {
+	return j.inner
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (j *ArgumentPresetValue) UnmarshalJSON(b []byte) error {
+	var rawValue map[string]any
+	if err := json.Unmarshal(b, &rawValue); err != nil {
+		return err
+	}
+
+	strType, err := getStringFromAnyMap(rawValue, "type")
+	if err != nil {
+		return fmt.Errorf("ArgumentPresetValue.type: %w", err)
+	}
+
+	valueType, err := ParseArgumentPresetValueType(strType)
+	if err != nil {
+		return fmt.Errorf("ArgumentPresetValue.type: %w", err)
+	}
+
+	switch valueType {
+	case ArgumentPresetValueTypeLiteral:
+		j.inner = &ArgumentPresetValueLiteral{
+			Type:  valueType,
+			Value: rawValue["value"],
+		}
+	case ArgumentPresetValueTypeEnv:
+		name, err := getStringFromAnyMap(rawValue, "name")
+		if err != nil {
+			return fmt.Errorf("ArgumentPresetValue.name: %w", err)
+		}
+
+		j.inner = &ArgumentPresetValueEnv{
+			Type: valueType,
+			Name: name,
+		}
+	case ArgumentPresetValueTypeForwardHeader:
+		name, err := getStringFromAnyMap(rawValue, "name")
+		if err != nil {
+			return fmt.Errorf("ArgumentPresetValue.name: %w", err)
+		}
+
+		j.inner = &ArgumentPresetValueForwardHeader{
+			Type: valueType,
+			Name: name,
+		}
+	}
+
+	return nil
+}
+
+// MarshalYAML implements yaml.Marshaler.
+func (j ArgumentPresetValue) MarshalYAML() (any, error) {
+	return j.inner, nil
+}
+
+// UnmarshalYAML implements yaml.Unmarshaler.
+func (j *ArgumentPresetValue) UnmarshalYAML(value *yaml.Node) error {
+	var rawType string
+	for i, node := range value.Content {
+		if node.Value != "type" {
+			continue
+		}
+		if len(value.Content) > i+1 {
+			rawType = value.Content[i+1].Value
+		}
+
+		break
+	}
+
+	valueType, err := ParseArgumentPresetValueType(rawType)
+	if err != nil {
+		return fmt.Errorf("ArgumentPresetValue.type: %w", err)
+	}
+
+	switch valueType {
+	case ArgumentPresetValueTypeLiteral:
+		var result ArgumentPresetValueLiteral
+		if err := value.Decode(&result); err != nil {
+			return err
+		}
+		j.inner = &result
+	case ArgumentPresetValueTypeEnv:
+		var result ArgumentPresetValueEnv
+		if err := value.Decode(&result); err != nil {
+			return err
+		}
+		j.inner = &result
+	case ArgumentPresetValueTypeForwardHeader:
+		var result ArgumentPresetValueForwardHeader
+		if err := value.Decode(&result); err != nil {
+			return err
+		}
+		j.inner = &result
+	}
+
+	return nil
+}
+
+// MarshalJSON implements json.Marshaler.
+func (j ArgumentPresetValue) MarshalJSON() ([]byte, error) {
+	return json.Marshal(j.inner)
+}
+
+// JSONSchema is used to generate a custom jsonschema
+func (j ArgumentPresetValue) JSONSchema() *jsonschema.Schema {
+	return &jsonschema.Schema{
+		OneOf: []*jsonschema.Schema{
+			ArgumentPresetValueLiteral{}.JSONSchema(),
+			ArgumentPresetValueEnv{}.JSONSchema(),
+			ArgumentPresetValueForwardHeader{}.JSONSchema(),
+		},
+	}
+}
+
+// ArgumentPresetValueInterface abstracts an interface for ArgumentPresetValue.
+type ArgumentPresetValueInterface interface {
+	GetType() ArgumentPresetValueType
+}
+
+// ArgumentPresetValueLiteral represents an literal argument preset value.
+type ArgumentPresetValueLiteral struct {
+	Type  ArgumentPresetValueType `json:"type"  mapstructure:"type"  yaml:"type"`
+	Value any                     `json:"value" mapstructure:"value" yaml:"value"`
+}
+
+// JSONSchema is used to generate a custom jsonschema
+func (j ArgumentPresetValueLiteral) JSONSchema() *jsonschema.Schema {
+	properties := orderedmap.New[string, *jsonschema.Schema]()
+	properties.Set("type", &jsonschema.Schema{
+		Type: "string",
+		Enum: []any{ArgumentPresetValueTypeLiteral},
+	})
+
+	properties.Set("value", &jsonschema.Schema{})
+
+	return &jsonschema.Schema{
+		Type:       "object",
+		Properties: properties,
+		Required:   []string{"type", "value"},
+	}
+}
+
+// GetType gets the type of the current argument preset value.
+func (apv ArgumentPresetValueLiteral) GetType() ArgumentPresetValueType {
+	return apv.Type
+}
+
+// ArgumentPresetValueEnv represents an environment argument preset value.
+type ArgumentPresetValueEnv struct {
+	Type ArgumentPresetValueType `json:"type" mapstructure:"type" yaml:"type"`
+	Name string                  `json:"name" mapstructure:"name" yaml:"name"`
+}
+
+// JSONSchema is used to generate a custom jsonschema
+func (j ArgumentPresetValueEnv) JSONSchema() *jsonschema.Schema {
+	properties := orderedmap.New[string, *jsonschema.Schema]()
+	properties.Set("type", &jsonschema.Schema{
+		Type: "string",
+		Enum: []any{ArgumentPresetValueTypeEnv},
+	})
+
+	properties.Set("name", &jsonschema.Schema{
+		Type: "string",
+	})
+
+	return &jsonschema.Schema{
+		Type:       "object",
+		Properties: properties,
+		Required:   []string{"type", "name"},
+	}
+}
+
+// GetType gets the type of the current argument preset value.
+func (j ArgumentPresetValueEnv) GetType() ArgumentPresetValueType {
+	return j.Type
+}
+
+// ArgumentPresetValueForwardHeader represents an argument preset value config from header forwarding.
+type ArgumentPresetValueForwardHeader struct {
+	Type ArgumentPresetValueType `json:"type" mapstructure:"type" yaml:"type"`
+	Name string                  `json:"name" mapstructure:"name" yaml:"name"`
+}
+
+// JSONSchema is used to generate a custom jsonschema
+func (j ArgumentPresetValueForwardHeader) JSONSchema() *jsonschema.Schema {
+	properties := orderedmap.New[string, *jsonschema.Schema]()
+	properties.Set("type", &jsonschema.Schema{
+		Type: "string",
+		Enum: []any{ArgumentPresetValueTypeForwardHeader},
+	})
+
+	properties.Set("name", &jsonschema.Schema{
+		Type: "string",
+	})
+
+	return &jsonschema.Schema{
+		Type:       "object",
+		Properties: properties,
+		Required:   []string{"type", "name"},
+	}
+}
+
+// GetType gets the type of the current argument preset value.
+func (apv ArgumentPresetValueForwardHeader) GetType() ArgumentPresetValueType {
+	return apv.Type
+}
+
 // parseHttpURL parses and validate if the URL has HTTP scheme
 func parseHttpURL(input string) (*url.URL, error) {
 	if !strings.HasPrefix(input, "https://") && !strings.HasPrefix(input, "http://") {
@@ -106,4 +384,18 @@ func ParseRelativeOrHttpURL(input string) (*url.URL, error) {
 	}
 
 	return parseHttpURL(input)
+}
+
+func getStringFromAnyMap(input map[string]any, key string) (string, error) {
+	rawValue, ok := input[key]
+	if !ok {
+		return "", errors.New("field is required")
+	}
+
+	strValue, ok := rawValue.(string)
+	if !ok {
+		return "", fmt.Errorf("expected string, got: %v", rawValue)
+	}
+
+	return strValue, nil
 }
